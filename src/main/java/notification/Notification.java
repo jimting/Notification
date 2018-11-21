09bc17633a1272bec353018f319521bedb6e1901
jimting <handsome841206@gmail.com>
2018-11-21 21:49:49 +0800

    test

--- src/main/java/notification/Notification.java
+++ src/main/java/notification/Notification.java
@@ -4,7 +4,7 @@ public class Notification {
 	public static String getNotification(String userID) 
 	{
 		//拿到所有的通知
-		String result = "[{\"content\":\"通知內容1\", \"status\":\"unread \"},{\"content\":\"通知內容2\", \"status\":\"unread \"},{\"content\":\"通知內容3\", \"status\":\"unread \"}]";
+		String result= "[{\"content\":\"通知內容1\", \"status\":\"unread \"},{\"content\":\"通知內容2\", \"status\":\"unread \"},{\"content\":\"通知內容3\", \"status\":\"unread \"}]";
 		return result;
 	}
 	
