6faedca703a31ee73b35af032ac5bc42e0ca6a5c
jimting <handsome841206@gmail.com>
2019-02-13 00:32:40 +0800

    update for swagger

--- src/main/java/Notification/NotificationController.java
+++ src/main/java/Notification/NotificationController.java
@@ -2,17 +2,21 @@ package Notification;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
-
-
-
+@Api(value = "GroceryInventoryController", tags = "與周邊商品相關的所有一切都在這裡")
 @RestController
 public class NotificationController {
+	
+	@ApiOperation(value = "測試此伺服器是否成功連線", notes = "成功連線就回傳success")
 	@CrossOrigin(origins = "*")
 	@RequestMapping("/")
     public String index() 
@@ -20,24 +24,27 @@ public class NotificationController {
 		return "success";
     }
 	
+	@ApiOperation(value = "拿到通知", notes = "回傳所有通知")
 	@CrossOrigin(origins = "*")
 	@RequestMapping(value = "getNotification", method = RequestMethod.GET)
-    public String getNotification(@RequestParam("userID") String userID)
+    public String getNotification(@ApiParam(required = true, name = "userID", value = "使用者編號") @RequestParam("userID") String userID)
     {
     	return Notification.getNotification(userID);
     }
 	
+	@ApiOperation(value = "新增通知", notes = "新增成功就回傳成功")
 	@CrossOrigin(origins = "*")
 	@RequestMapping(value = "newNotification", method = RequestMethod.GET)
-    public String newNotification(@RequestParam("userID") String userID, @RequestParam("content") String content)
+    public String newNotification(@ApiParam(required = true, name = "userID", value = "使用者編號") @RequestParam("userID") String userID,@ApiParam(required = true, name = "content", value = "通知內容") @RequestParam("content") String content)
     {
     	
     	return Notification.newNotification(userID, content);
     }
 	
+	@ApiOperation(value = "將通知標為已讀", notes = "標記成功就回傳成功")
 	@CrossOrigin(origins = "*")
 	@RequestMapping(value = "setNotificationRead", method = RequestMethod.GET)
-    public String setNotificationRead(@RequestParam("ID") String ID)
+    public String setNotificationRead(@ApiParam(required = true, name = "ID", value = "通知編號") @RequestParam("ID") String ID)
     {
     	Notification.setNotificationRead(ID);
     	return "success";
