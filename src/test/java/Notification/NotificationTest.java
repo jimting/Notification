216753b046a4b3e27a96393cb32198d7c6e1dff8
jimting <handsome841206@gmail.com>
2019-02-21 13:23:27 +0800

    update junit test

--- src/test/java/Notification/NotificationTest.java
+++ src/test/java/Notification/NotificationTest.java
@@ -1,15 +1,31 @@
 package Notification;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+ 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class NotificationTest {
-
+	private NotificationController notififation = new NotificationController();
+	
 	@Test
+	//測試是否成功連線到DB
 	public void testDatabase()
 	{
-		//測試是否成功連線到DB
 		//if(GroceryInventory.getGrocery(userID))System.out.println("答對了！");
 		//else	System.out.println("正確答案應該是5");
+	}
+	
+	@Test
+	//controller功能測試
+	public void testIndex()
+	{
+		assertEquals("success", notififation.index());
 	}
 
 }
