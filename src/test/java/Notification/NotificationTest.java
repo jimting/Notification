fdf3d23cf36c6b5d75a376f8a820fdcb142a5493
Chen <i11007020@gmail.com>
2019-07-12 18:22:09 +0800

    add annotations @Runwith, @SpringBootTest and @Autowired

--- src/test/java/Notification/NotificationTest.java
+++ src/test/java/Notification/NotificationTest.java
@@ -1,14 +1,23 @@
 package Notification;
 
-import static org.junit.Assert.*;
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class NotificationTest {
-	private NotificationController notification = new NotificationController();
+
+	@Autowired
+	private NotificationController notification;
 	
 	//test controller
 	
