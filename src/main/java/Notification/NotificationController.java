cc7a3f99cdfbe564d3ad518f2e33ffb6fc489139
Chen <i11007020@gmail.com>
2019-07-14 15:15:05 +0800

    delete FeignInterface

--- src/main/java/Notification/NotificationController.java
+++ src/main/java/Notification/NotificationController.java
@@ -1,23 +1,14 @@
 package Notification;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import org.springframework.web.bind.annotation.*;
 
 
 @Api(value = "NotificationController", tags = "與通知相關的所有一切都在這裡")
 @RestController
 public class NotificationController {
-	
-	@Autowired
-	FeignInterface feignInterface;
 	
 	@ApiOperation(value = "測試此伺服器是否成功連線", notes = "成功連線就回傳success")
 	@CrossOrigin(origins = "*")
