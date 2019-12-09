85995217aafb7d2a14d24a852ffeae1202323ad1
Jimting <handsome841206@gmail.com>
2019-12-09 19:23:51 +0800

    Update NotificationController.java

--- src/main/java/Notification/NotificationController.java
+++ src/main/java/Notification/NotificationController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.*;
 public class NotificationController {
 	
 	@ApiOperation(value = "測試此伺服器是否成功連線", notes = "成功連線就回傳success")
-	@CrossOrigin(origins = "*")
 	@RequestMapping(value = "/", method = RequestMethod.GET)
     public String index() 
     {
@@ -19,7 +18,6 @@ public class NotificationController {
     }
 	
 	@ApiOperation(value = "拿到通知", notes = "回傳所有通知")
-	@CrossOrigin(origins = "*")
 	@RequestMapping(value = "getNotification", method = RequestMethod.GET)
     public String getNotification(@ApiParam(required = true, name = "userID", value = "使用者編號") @RequestParam("userID") String userID)
     {
@@ -27,7 +25,6 @@ public class NotificationController {
     }
 	
 	@ApiOperation(value = "新增通知", notes = "新增成功就回傳成功")
-	@CrossOrigin(origins = "*")
 	@RequestMapping(value = "newNotification", method = RequestMethod.GET)
     public String newNotification(@ApiParam(required = true, name = "userID", value = "使用者編號") @RequestParam("userID") String userID,@ApiParam(required = true, name = "content", value = "通知內容") @RequestParam("content") String content)
     {
@@ -36,7 +33,6 @@ public class NotificationController {
     }
 	
 	@ApiOperation(value = "將通知標為已讀", notes = "標記成功就回傳成功")
-	@CrossOrigin(origins = "*")
 	@RequestMapping(value = "setNotificationRead", method = RequestMethod.GET)
     public String setNotificationRead(@ApiParam(required = true, name = "ID", value = "通知編號") @RequestParam("ID") String ID)
     {
@@ -45,7 +41,6 @@ public class NotificationController {
     }
 	
 	@ApiOperation(value = "刪除最後一則通知", notes = "刪除成功救回傳success")
-	@CrossOrigin(origins = "*")
 	@RequestMapping(value = "deleteNotification", method = RequestMethod.GET)
     public String deleteNotification(@ApiParam(required = true, name = "userID", value = "使用者編號") @RequestParam("userID") String ID)
     {
